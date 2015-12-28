140212b8db040049814fe855e377f41091f222d3
Joe Frew <JoeFrew95@gmail.com>
2015-12-28 17:23:51 +0000

    finished WeightedSynapse test cases and WeightdSynapse class

--- src/com/joefrew/neuralnet/WeightedSynapse.java
+++ src/com/joefrew/neuralnet/WeightedSynapse.java
@@ -3,12 +3,23 @@ package com.joefrew.neuralnet;
 public class WeightedSynapse extends Synapse {
 	
 	protected double weight = 1;
+	
+	public WeightedSynapse() {}
 
-	public double getInput() {
-		//return super.getInput() * this.weight;
-		return 0;
+	public WeightedSynapse(int weight) {
+		this.weight = weight;
 	}
 	
-	
+	public double getInput() {
+		return super.getInput() * this.weight;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
 
 }
