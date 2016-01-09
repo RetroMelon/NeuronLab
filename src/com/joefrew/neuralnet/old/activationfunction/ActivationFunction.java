b7cd9782e2f90d29d5557937e83ddde7d2ffc61a
Joe Frew <JoeFrew95@gmail.com>
2016-01-09 01:49:37 +0000

    removed all old neural network code. it is in the commit history so we can still get it back.

--- src/com/joefrew/neuralnet/old/activationfunction/ActivationFunction.java
+++
@@ -1,7 +0,0 @@
-package com.joefrew.neuralnet.old.activationfunction;
-
-public interface ActivationFunction {
-	
-	public double activate(double input);
-
-}
--- src/com/joefrew/neuronlab/Main.java
+++ src/com/joefrew/neuronlab/Main.java
@@ -1,16 +1,6 @@
 package com.joefrew.neuronlab;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
-
 import com.joefrew.neuralnet.BiasNetwork;
-import com.joefrew.neuralnet.genetics.Combiner;
-import com.joefrew.neuralnet.genetics.DummyRandom;
-import com.joefrew.neuralnet.old.ConnectedNetwork;
-import com.joefrew.neuralnet.old.InputNeuron;
-import com.joefrew.neuralnet.old.Neuron;
 import com.joefrew.neuronlab.fishexperiment.FishExperiment;
 
 public class Main {
@@ -20,10 +10,6 @@ public class Main {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		BiasNetwork network = new BiasNetwork();
-		
-		
-		
 		new FishExperiment().run();
 	}
 
